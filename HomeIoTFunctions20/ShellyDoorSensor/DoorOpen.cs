52d46d161e6f4e3afd9fd93a4adf71b69ec5f00e
Leivo Sepp <leivo@ekstreem.ee>
2021-02-11 16:43:12 +0200

    Adding two Door functions - testing.

---
+++ HomeIoTFunctions20/ShellyDoorSensor/DoorOpen.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace HomeIoTFunctions20.ShellyDoorSensor
+{
+    public static class DoorOpen
+    {
+        [FunctionName("DoorOpen")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "FreeCosmosDB",
+                collectionName: "TelemetryData",
+                ConnectionStringSetting = "CosmosDBConnection"
+                )]
+                IAsyncCollector<dynamic> output,
+            ExecutionContext context,
+            ILogger log)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var sendData = new 
+            {
+                DateAndTime = GetEnergyMarketPrice.GetEnergyMarketPrice.DateTimeTZ(),
+                Door = "Open"
+            };
+            await output.AddAsync(sendData);
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
